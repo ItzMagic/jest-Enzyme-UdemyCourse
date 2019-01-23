99bc5157ab8ba4cd0055aff8c0a842dd2be37c95
Evan Londono <esodemoneyes@gmail.com>
2019-01-23 11:05:27 -0500

    more mock test

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EznymeAdapter from 'enzyme-adapter-react-16';
+
 import App from './App';
 
 Enzyme.configure({adapter: new EznymeAdapter()});
 
 test('renders without crashing', () => {
-  const wrapper = shallow(<App/>);
-  console.log(wrapper.debug());
+  const wrapper = shallow(<App/>);  
+  const appComponent = wrapper.find("['data-test='component-app']")
 } );
+test('renders increment button', ()=>{
+
+});
+
+test('renders counter display', ()=> {
+
+});
+test('counter starts at 0', () => {
+
+});
+test('clicking button increments counter display', () =>{
+  
+})
